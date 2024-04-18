9f3429a03ceb5cdb42d78214da3743c3d5360d01
Georgi Kostov <georgi.kostov@egt.com>
2024-04-18 17:59:58 +0300

    add Glad + Shader initial setup

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,47 @@
 workspace "Breakout"
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 	configurations { "Debug", "Release" }
     platforms { "x64" }
 
+    IncludeDir = {}
+    IncludeDir["Glad"] = "Glad/include"
+
+    include "Glad"
+
 project "Breakout"
-	kind "ConsoleApp"
+    location "Breakout"
+    kind "ConsoleApp"
 	language "C++"
+        
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    links 
+    {
+        "Glad",
+        "opengl32.lib"
+    }
+
+	
     targetdir "bin/%{cfg.buildcfg}"
 
-    includedirs { "src" }
+    includedirs 
+    {
+        "src",
+        "%{IncludeDir.Glad}"
+    }
 
     filter { "platforms:x64" }
     system "Windows"
     architecture "x86_64"
 
-    files { "**.h", "**.cpp" }
+    files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
 
     filter "configurations:Debug"
     defines { "DEBUG" }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,68 @@
-int main()
+#include "Core\Shader\Shader.h"
+
+#include <iostream>
+
+void CreateVertexShader(const char* source)
 {
+	unsigned int vertexShader;
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &source, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char log[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, log);
+		std::cout << "[Error]: Failed to compile [--VERTEX--] shader. Reason: " << log << std::endl;
+	}
+}
+
+void CreateFragmentShader(const char* source)
+{
+	unsigned int fragmentShader;
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &source, NULL);
+	glCompileShader(fragmentShader);
+
+	int success;
+	char log[512];
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, log);
+		std::cout << "[Error]: Failed to compile [--FRAGMENT--] shader. Reason: " << log << std::endl;
+	}
+}
+
+void CreateGeometryShader(const char* source)
+{
+	unsigned int geometryShader;
+
+	geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+	glShaderSource(geometryShader, 1, &source, NULL);
+	glCompileShader(geometryShader);
+
+	int success;
+	char log[512];
+
+	glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(geometryShader, 512, NULL, log);
+		std::cout << "[Error]: Failed to compile [--GEOMETRY--] shader. Reason: " << log << std::endl;
+	}
+}
+
+int main()
+{	
+
 	return 0;
 }
